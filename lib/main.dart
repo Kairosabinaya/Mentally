e0002c37eb7b542696f32c39891e6619c474e2b2
Admantix <williamwijaya499@gmail.com>
2025-06-08 18:54:20 +0700

    Implement authentication pages and models; replace JournalingPage with LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mentally/page/journaling.dart';
+import 'package:mentally/page/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mentally',
-      home: JournalingPage(),
+      home: LoginPage(),
     );
   }
 }
--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class WelcomePage extends StatelessWidget {
-  const WelcomePage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
